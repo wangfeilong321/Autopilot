84afc2b5dc2da0915063633a77bf2a13054ab133
Elia <elias.tarasov@gmail.com>
2015-12-29 12:33:06 +0300

    TODO: Add chain continuation into doRead() and doWrite() funcs.

--- src/InputOutput/SocketBoard.cpp
+++ src/InputOutput/SocketBoard.cpp
@@ -32,47 +32,32 @@ bool SocketBoard::Connected() {
 }
 
 bool SocketBoard::Run() {
-	doRead();
 	doWrite();
+	doRead();
 	return true;
 }
 
 void SocketBoard::doRead() {
-	create_task(reader->LoadAsync(sizeof(UINT32))).then([this](UINT32 size) {
-		if (size != sizeof(UINT32)) {
+	/*
+	task<UINT32>(reader->LoadAsync(sizeof(UINT32)).then([this](UINT32 size) {
+		if (size < sizeof(UINT32)) {
 			// The underlying socket was closed before we were able to read the whole data.
 			cancel_current_task();
 		}
 		UINT32 dataLength = reader->ReadUInt32();
-		return create_task(reader->LoadAsync(dataLength)).then([this, dataLength](UINT32 actualDataLength) {
+		task<UINT32>(reader->LoadAsync(dataLength)).then([this, dataLength](UINT32 actualDataLength) {
 			if (actualDataLength != dataLength) {
 				// The underlying socket was closed before we were able to read the whole data.
 				cancel_current_task();
 			}
 			//read data here
-			DOUBLE aileronCmd = reader->ReadDouble(); 
-			DOUBLE elevatorCmd = reader->ReadDouble();
-			DOUBLE rudderCmd = reader->ReadDouble();
-			DOUBLE throttleCmd = reader->ReadDouble();
+			reader->ReadDouble(); //aileron
+			reader->ReadDouble(); //elevator
+			reader->ReadDouble(); //rudder
+			reader->ReadDouble(); //throttle
 		});
-	}).then([this](task<void> previousTask) {
-		try {
-			// Try getting all exceptions from the continuation chain above this point.
-			previousTask.get();
-			// Everything went ok, so try to receive another string. The receive will continue until the stream is
-			// broken (i.e. peer closed closed the socket).
-		}
-		catch (Exception^ exception) {
-			String^ message = "Read stream failed with error: " + exception->Message;
-			// Explicitly close the socket.
-			delete socket;
-		}
-		catch (task_canceled&) {
-			// Do not print anything here - this will usually happen because user closed the client socket.
-			// Explicitly close the socket.
-			delete socket;
-		}
 	});
+	*/
 }
 
 void SocketBoard::doWrite() {
@@ -86,15 +71,8 @@ void SocketBoard::doWrite() {
 
 	auto totalMessageSize = sizeof(UINT32) + 6 * sizeof(DOUBLE); //total message size
 
-	create_task(writer->StoreAsync()).then([this, totalMessageSize](task<UINT32> writeTask) {
-		try {
-			// Try getting an exception.
-			auto writtenBytes = writeTask.get();
-			if (writtenBytes != totalMessageSize)
-				cancel_current_task();
-		}
-		catch (Exception^ exception) {
-			String^ message = "Send failed with error: " + exception->Message;
-		}
+	task<UINT32>(writer->StoreAsync()).then([this, totalMessageSize](UINT32 writtenBytes) {
+		if (writtenBytes != totalMessageSize)
+			cancel_current_task();
 	});
 }
